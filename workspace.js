93723c50168e04c7f03481ec6c638badada021b3
Kanishktyagia1 <kanishktyagi57@gmail.com>
2026-08-26 20:31:15 +0530

    Update workspace.js

--- workspace.js
+++ workspace.js
@@ -1,4 +1,4 @@
-const editor = document.getElementById("codeEditor");
+alert("Workspace JS loaded");const editor = document.getElementById("codeEditor");
 const preview = document.getElementById("preview");
 
 const tabs = document.querySelectorAll(".editor-tab");
