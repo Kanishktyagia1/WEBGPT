077c785468d857f56448eddf1b6ed852f42f58b7
Kanishktyagia1 <kanishktyagi57@gmail.com>
2026-08-27 18:15:24 +0530

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,1 +1,27 @@
+const preview = document.getElementById("websitePreview");
+const backBtn = document.getElementById("backBtn");
 
+const savedProject = localStorage.getItem("webgpta1_project");
+
+if (savedProject) {
+
+    const code = JSON.parse(savedProject);
+
+    const html = code.html || "";
+    const css = `<style>${code.css || ""}</style>`;
+    const js = `<script>${code.js || ""}<\/script>`;
+
+    preview.srcdoc = html + css + js;
+
+} else {
+
+    preview.srcdoc = `
+        <h2 style="font-family:Arial;text-align:center;margin-top:50px">
+            No website generated yet.
+        </h2>
+    `;
+}
+
+backBtn.addEventListener("click", () => {
+    window.location.href = "workspace.html";
+});
